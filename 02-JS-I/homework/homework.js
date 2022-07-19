57431abeab2f5d24622023020a0e56157df4c254
Eduardo Helfer <eduardohelfer@gmx.net>
2022-07-19 12:25:47 -0500

    test2

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = "Holas";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 2;
+const nuevoNum = "how are you";
 
 // Crea una variable booleana:
 const nuevoBool = 1 === 1;
